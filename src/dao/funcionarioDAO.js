b46c89712a2764682237f34eb9065ad71bed2f5f
Nelson <punkpunkada@gmail.com>
2021-11-30 17:56:19 -0300

    creating funcionarioDAO

--- src/dao/funcionarioDAO.js
+++ src/dao/funcionarioDAO.js
@@ -9,7 +9,7 @@ class FuncionarioDAO{
     insert = (newfunc) =>{
 
         return new Promise((resolve, reject) =>{
-            this.conexao.query(``)
+            this.conexao.query(`INSERT INTO FUNCIONARIOS `)
         })
 
         /*
