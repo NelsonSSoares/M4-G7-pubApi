4f883332b0272a2b00ae21b3a16cb508ff4d68b7
Bárbara Cruz <ba.luizacruz@gmail.com>
2022-01-13 22:28:16 -0300

    fix

--- src/dao/comidaDAO.js
+++ src/dao/comidaDAO.js
@@ -47,7 +47,7 @@ class ComidaDAO{
     updateComida(id, comida){
         console.log(id)
         return new Promise((resolve, reject) => {
-            this.conexao.query(`UPDATE comidas SET nome = ? , descricao = ?, preco = ?, , imagem = ?  WHERE id = ?`, [ comida.nome, comida.descricao, comida.preco, comida.imagem, id  ],
+            this.conexao.query(`UPDATE comidas SET nome = ? , descricao = ?, preco = ?, imagem = ?  WHERE id = ?`, [ comida.nome, comida.descricao, comida.preco, comida.imagem, id  ],
             (error) => {
                 if (error) {
                     reject('Erro ao atualizar: ' + error)
